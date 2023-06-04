cec6ce6cff624d31834a3afa4790da14d99aacb6
shubh batra <godwithsam@gmail.com>
2023-06-04 17:39:22 +0100

    start: Added navbar for the file.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,118 @@
-import React from 'react'
+import Link from "next/link";
+import Logo from "../../assets/logo/Logo.png";
+import { useState } from "react";
 
-const Navbar = () => {
+function Navbar() {
+  const [nav, setNav] = useState(false);
+
+  const openNav = () => {
+    setNav(!nav);
+  };
+
   return (
-    <div>Navbar</div>
-  )
+    <>
+      <nav>
+        {/* mobile */}
+        <div className={`mobile-navbar ${nav ? "open-nav" : ""}`}>
+          <div onClick={openNav} className="mobile-navbar__close">
+            <i className="fa-solid fa-xmark"></i>
+          </div>
+          <ul className="mobile-navbar__links">
+            <li>
+              <Link onClick={openNav} href="/">
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link onClick={openNav} href="/about">
+                About
+              </Link>
+            </li>
+            <li>
+              <Link onClick={openNav} href="/models">
+                Models
+              </Link>
+            </li>
+            <li>
+              <Link onClick={openNav} href="/testimonials">
+              Reviews
+              </Link>
+            </li>
+            <li>
+              <Link onClick={openNav} href="/team">
+                The Team
+              </Link>
+            </li>
+            <li>
+              <Link onClick={openNav} href="/contact">
+                Contact
+              </Link>
+            </li>
+          </ul>
+        </div>
+
+        {/* desktop */}
+
+        <div className="navbar">
+          <div className="navbar__img">
+            <Link href="/" onClick={() => window.scrollTo(0, 0)}>
+              <img src={Logo} alt="logo-img" />
+            </Link>
+          </div>
+          <ul className="navbar__links">
+            <li>
+              <Link className="home-link" href="/">
+                Home
+              </Link>
+            </li>
+            <li>
+              {" "}
+              <Link className="about-link" href="/about">
+                About
+              </Link>
+            </li>
+            <li>
+              {" "}
+              <Link className="models-link" href="/models">
+                Vehicle Models
+              </Link>
+            </li>
+            <li>
+              {" "}
+              <Link className="testi-link" href="/testimonials">
+                Reviews
+              </Link>
+            </li>
+            <li>
+              {" "}
+              <Link className="team-link" href="/team">
+                The Team
+              </Link>
+            </li>
+            <li>
+              {" "}
+              <Link className="contact-link" href="/contact">
+                Contact
+              </Link>
+            </li>
+          </ul>
+          <div className="navbar__buttons">
+            <Link className="navbar__buttons__sign-in" href="/">
+              Sign In
+            </Link>
+            <Link className="navbar__buttons__register" href="/">
+              Register
+            </Link>
+          </div>
+
+          {/* mobile */}
+          <div className="mobile-hand" onClick={openNav}>
+            <i className="fa-solid fa-bars"></i>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
 }
 
 export default Navbar;
