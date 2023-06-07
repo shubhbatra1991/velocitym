34a604e554a15b1d90965be9743fe38d8feb113b
shubh batra <godwithsam@gmail.com>
2023-06-07 19:54:50 +0100

    mid: created Hero page.

--- src/components/Fleet.js
+++ src/components/Fleet.js
@@ -1,9 +1,143 @@
-import React from 'react'
+import React from 'react';
+import { useState } from "react";
+import CarBox from "./CarBox";
+import { CAR_DATA } from "./CarData";
 
 const Fleet = () => {
-  return (
-    <div>Fleet</div>
-  )
-}
 
-export default Fleet
+  const [active, setActive] = useState("SecondCar");
+  const [colorBtn, setColorBtn] = useState("btn1");
+
+  const btnID = (id) => {
+    setColorBtn(colorBtn === id ? "" : id);
+  };
+
+  const coloringButton = (id) => {
+    return colorBtn === id ? "colored-button" : "";
+  };
+
+
+  return (
+
+    <>
+      <section className="py-[10rem]">
+        <div className="container">
+          <div className="flex flex-col">
+            <div className="m-0 text-center text-black max-w-[50rem] mb-[5rem]">
+              <h3 className='text-[3rem] font-mont font-semibold'>Vehicle Models</h3>
+              <h2 className='text-[2rem] font-mont font-semibold'>Our rental fleet</h2>
+              <p className='text-[1.2rem] font-mont text-gray-400 leading-[.5]'>
+                Choose from a variety of our amazing vehicles to rent for your
+                next adventure or business trip
+              </p>
+            </div>
+            <div className="flex justify-between gap-[1rem] lg:flex-col lg:gap-[5rem]">
+              {/* pick car */}
+              <div className="flex flex-col gap-[0.7rem]">
+                <button
+                  className={`
+                    ${coloringButton("btn1")}
+                    text-[2rem] font-mont font-semibold cursor-pointer border-0
+                    px-[1.5rem] py-[2.5rem] bg-white transition-all duration-200
+                    text-left hover:bg-orange-500 hover:text-white
+                    `}
+                  onClick={() => {
+                    setActive("SecondCar");
+                    btnID("btn1");
+                  }}
+                >
+                  Audi A1 S-Line
+                </button>
+                <button
+                  className={`
+                  ${coloringButton("btn2")}
+                  text-[2rem] font-mont font-semibold cursor-pointer border-0
+                    px-[1.5rem] py-[2.5rem] bg-white transition-all duration-200
+                    text-left hover:bg-orange-500 hover:text-white
+                  `}
+                  id="btn2"
+                  onClick={() => {
+                    setActive("FirstCar");
+                    btnID("btn2");
+                  }}
+                >
+                  VW Golf 6
+                </button>
+                <button
+                  className={`
+                    text-[2rem] font-mont font-semibold cursor-pointer border-0
+                    px-[1.5rem] py-[2.5rem] bg-white transition-all duration-200
+                    text-left hover:bg-orange-500 hover:text-white
+                    ${coloringButton("btn3")}
+                    `}
+                  id="btn3"
+                  onClick={() => {
+                    setActive("ThirdCar");
+                    btnID("btn3");
+                  }}
+                >
+                  Toyota Camry
+                </button>
+                <button
+                  className={`
+                  ${coloringButton("btn4")}
+                  text-[2rem] font-mont font-semibold cursor-pointer border-0
+                    px-[1.5rem] py-[2.5rem] bg-white transition-all duration-200
+                    text-left hover:bg-orange-500 hover:text-white
+                  `}
+                  id="btn4"
+                  onClick={() => {
+                    setActive("FourthCar");
+                    btnID("btn4");
+                  }}
+                >
+                  BMW 320 ModernLine
+                </button>
+                <button
+                  className={`
+                  ${coloringButton("btn5")}
+                  text-[2rem] font-mont font-semibold cursor-pointer border-0
+                    px-[1.5rem] py-[2.5rem] bg-white transition-all duration-200
+                    text-left hover:bg-orange-500 hover:text-white
+                  `}
+                  id="btn5"
+                  onClick={() => {
+                    setActive("FifthCar");
+                    btnID("btn5");
+                  }}
+                >
+                  Mercedes-Benz GLK
+                </button>
+                <button
+                  className={`
+                  text-[2rem] font-mont font-semibold cursor-pointer border-0
+                    px-[1.5rem] py-[2.5rem] bg-white transition-all duration-200
+                    text-left hover:bg-orange-500 hover:text-white
+                    ${coloringButton("btn6")}
+                  `}
+                  id="btn6"
+                  onClick={() => {
+                    setActive("SixthCar");
+                    btnID("btn6");
+                  }}
+                >
+                  VW Passat CC
+                </button>
+              </div>
+
+              {active === "FirstCar" && <CarBox data={CAR_DATA} carID={0} />}
+              {active === "SecondCar" && <CarBox data={CAR_DATA} carID={1} />}
+              {active === "ThirdCar" && <CarBox data={CAR_DATA} carID={2} />}
+              {active === "FourthCar" && <CarBox data={CAR_DATA} carID={3} />}
+              {active === "FifthCar" && <CarBox data={CAR_DATA} carID={4} />}
+              {active === "SixthCar" && <CarBox data={CAR_DATA} carID={5} />}
+            </div>
+          </div>
+        </div>
+      </section>
+
+    </>
+  )
+};
+
+export default Fleet;
--- src/components/HeroBg.js
+++ src/components/HeroBg.js
@@ -1,8 +1,93 @@
-import React from 'react'
+import React from 'react';
+import Link from 'next/link';
+import BgShape from "../assets/hero/hero-bg.png";
+import HeroCar from "../assets/cars/car-front.png";
+import { useEffect, useState } from "react";
 
 const HeroBg = () => {
+  const [goUp, setGoUp] = useState(false);
+
+  const scrollToTop = () => {
+    window.scrollTo({ top: (0, 0), behavior: "smooth" });
+  };
+
+  const bookBtn = () => {
+    document
+      .querySelector("#booking-section")
+      .scrollIntoView({ behavior: "smooth" });
+  };
+
+  useEffect(() => {
+    const onPageScroll = () => {
+      if (window.scrollY > 600) {
+        setGoUp(true);
+      } else {
+        setGoUp(false);
+      }
+    };
+    window.addEventListener("scroll", onPageScroll);
+
+    return () => {
+      window.removeEventListener("scroll", onPageScroll);
+    };
+  }, []);
+
   return (
-    <div>HeroBg</div>
+    <>
+      <section id="home" className="w-full h-[97vh] bg-gray-300 relative">
+        <div className="container">
+          <img className="absolute top-0 right-0 z-1 md:hidden" src={BgShape} alt="bg-shape" />
+          <div className="w-full h-[100vh] flex items-center relative md:justify-center">
+            <div className="flex flex-col z-3 max-w-[50rem] mt-[5rem] md:text-center md:items-center">
+              <h4 className='text-[1.5rem] text-black font-mont'>Plan your trip now</h4>
+              <h1 className='text-xl font-mont font-bold text-black leading-[1.2] mt-[1rem] mb-[2.3rem]'>
+                Save <span className='text-orange-500'>big</span> with our car rental
+              </h1>
+              <p className='font-mont text-[1.2rem] leading-[1.6] text-gray-500 mb-[4rem]'>
+                Rent the car of your dreams. Unbeatable prices, unlimited miles,
+                flexible pick-up options and much more.
+              </p>
+              <div className="flex gap-8 text-[1.2rem] font-mont xs:flex-col">
+                <Link
+                  onClick={bookBtn}
+                  className="bg-orange-500 p-8 rounded-[0.3rem] transition-all duration-300 border-2 border-orange-500
+                  xs:p-4 hover:bg-orange-400 hover:shadow-lg hover:shadow-red-950 "
+                  href="/"
+                >
+                  Book Ride &nbsp; <i className="fa-solid fa-circle-check"></i>
+                </Link>
+                <Link className="text-black p-4 rounded-[0.3rem] border-2 border-black transition-all duration-300
+                hover:bg-transparent hover:text-black" href="/">
+                  Learn More &nbsp; <i className="fa-solid fa-angle-right"></i>
+                </Link>
+              </div>
+            </div>
+
+            {/* img */}
+            <img
+              src={HeroCar}
+              alt="car-img"
+              className="z-2 absolute right-0 w-[65%] mt-[5rem] md:hidden"
+            />
+          </div>
+        </div>
+
+        {/* page up */}
+        <div
+          onClick={scrollToTop}
+          className={
+            `
+            fixed text-[2.5rem] text-white bg-orange-600 w-[2rem]
+            h-[2rem] bottom-[5rem] right-[5rem] z-20 hidden items-center text-center
+            justify-center p-8 cursor-pointer border-[3px] border-white
+            scroll-up 
+            ${goUp ? "show-scroll flex" : ""}
+            `}
+        >
+          <i className="fa-solid fa-angle-up"></i>
+        </div>
+      </section>
+    </>
   )
 }
 
