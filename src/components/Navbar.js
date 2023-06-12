201b0e4aa0ae552730e3ae5655cfd68bd1ea05d2
shubh batra <godwithsam@gmail.com>
2023-06-12 15:24:30 +0100

    Made some changes to nav.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,128 +1,111 @@
 import Link from "next/link";
+import Image from "next/image";
 import Logo from "../assets/logo/Logo.png";
 import { useState } from "react";
-import Image from "next/image";
-import { FaBars, FaTimes } from "react-icons/fa";
+import { useRouter } from 'next/router';
+
+
+const CustomLink = ({ href, title, className="" }) => {
+  const router= useRouter();
+
+  return(
+    <Link href={href} className={`${className} relative group`}>
+      {title}
+      <span className={`
+          h-[1px] inline-block w-full bg-dark absolute left-0 -bottom-0.5
+          group-hover:w-full transition-[width] ease duration-300
+          ${router.asPath === href ? 'w-full': 'w-0'}
+        `}
+      >
+      &nbsp;
+      </span>
+    </Link>
+
+  );
+}
+
+// for mobile view
+const CustomMobileLink = ({ href, title, className="", toggle })  => {
+
+  const router = useRouter();
+  const handleClick = () => {
+    toggle();
+    router.push(href)
+  };
+
+  return(
+    <button href={href} className={`${className} relative group text-light my-2`} onClick={handleClick}>
+            {title}
+
+            <span className={`
+                  h-[1px] inline-block w-0 bg-light 
+                  absolute left-0 -bottom-0.5
+                  group-hover:w-full transition-[width] ease duration-300
+                  ${router.asPath === href ? 'w-full': 'w-0'}
+              `}
+            >
+              &nbsp;
+            </span>
+        </button>
+  );
+}
+
 
 function Navbar() {
-  const [nav, setNav] = useState(false);
-
-  const openNav = () => {
-    setNav(!nav);
+  const [isOpen, setIsOpen] = useState(false);
+  const handleClick= () => {
+      setIsOpen(!isOpen)
   };
 
   return (
     <>
-      <nav>
-        {/* mobile */}
-        <div
-          className={`flex flex-col w-full h-[100vh] fixed top-0 left-[-100%] bg-white z-50 justify-center items-center transition-all duration-500 ease-in-out ${
-            nav ? "open-nav" : ""
-          }`}
+      <header  className='w-full px-32 py-8 font-medium flex items-center justify-between
+    relative z-10 lg:px-16 md:px-12 sm:px-8'>
+
+        <button className='flex-col justify-center items-center hidden lg:flex' onClick={handleClick}>
+            <span className={`bg-dark block transition-all duration-300 ease-out h-0.5 w-6 rounded-sm  ${isOpen ? 'rotate-45 translate-y-1': '-translate-y-0.5'}`}></span>
+            <span className={`bg-dark block transition-all duration-300 ease-out h-0.5 w-6 rounded-sm my-0.5 ${isOpen ? 'opacity-0': 'opacity-100'} `}></span>
+            <span className={`bg-dark transition-all duration-300 ease-out h-0.5 w-6 rounded-sm  ${isOpen ? '-rotate-45 -translate-y-1': 'translate-y-0.5'} `}></span>
+        </button>
+
+        <div className='w-full flex justify-between items-center'>
+          <nav className="text-black font-bold">
+              <CustomLink href="/" title="Home" className='mr-4' />
+              <CustomLink href="/about" title="About" className='mr-4' />
+              <CustomLink href="/models" title="Models" className='mr-4' />
+              <CustomLink href="/TestimonialsPage" title="Testimonials" className='mr-4' />
+              <CustomLink href="/team" title="Team" className='mr-4' />
+              <CustomLink href="/contact" title="Contact" className='mr-4' />
+          </nav>
+        </div>
+
+        {/* for mobile menu */}
+        {
+          isOpen ?
+          <div
+          initial={{scale:0, opacity:0, x:"-50%", y:"-50%"}}
+          animate={{scale:1, opacity:1}}
+          className='min-w-[70vw] flex flex-col z-30 justify-between 
+          items-center fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2
+          bg-dark/90 -light/75 rounded-lg backdrop-blur-md py-32
+          '
         >
-          <div
-            onClick={openNav}
-            className="text-3xl absolute top-[3.5rem] right-[3.5rem] cursor-pointer transition-all duration-300 hover:text-orange"
-          >
-            <FaTimes />
-          </div>
-          <ul className="flex flex-col list-none text-[2.3rem] gap-[3rem] text-center">
-            <li>
-              <Link onClick={openNav} href="/">
-                Home
-              </Link>
-            </li>
-            <li>
-              <Link onClick={openNav} href="/about">
-                About
-              </Link>
-            </li>
-            <li>
-              <Link onClick={openNav} href="/models">
-                Models
-              </Link>
-            </li>
-            <li>
-              <Link onClick={openNav} href="/TestimonialsPage">
-                Testimonials
-              </Link>
-            </li>
-            <li>
-              <Link onClick={openNav} href="/team">
-                Our Team
-              </Link>
-            </li>
-            <li>
-              <Link onClick={openNav} href="/contact">
-                Contact
-              </Link>
-            </li>
-          </ul>
+        <nav className='flex items-center flex-col justify-center'>
+                <CustomMobileLink href="/" title="Home" className='' toggle={handleClick} />
+                <CustomMobileLink href="/about" title="About" className='' toggle={handleClick} />
+                <CustomMobileLink href="/models" title="Models" className='' toggle={handleClick} />
+                <CustomMobileLink href="/TestimonialsPage" title="Testimonials" className='' toggle={handleClick} />
+                <CustomMobileLink href="/team" title="Team" className='' toggle={handleClick} />
+                <CustomMobileLink href="/contact" title="Contact" className='' toggle={handleClick} />
+            </nav>
+        </div>
+          : null
+        }
+        <div className='absolute top-2 translate-x-[-50%]'>
+          <Image src={Logo} alt="logo-img" width={100} height={100} />
         </div>
 
-        {/* desktop */}
-        <div className="max-w-[133rem] w-full h-auto flex justify-between items-center px-[2.7rem] py-[2rem] absolute top-0 left-0 right-0 bg-transparent z-50 mx-auto">
-          <div className="w-[14.5rem]">
-            <Link href="/" onClick={() => window.scrollTo(0, 0)}>
-              <Image src={Logo} alt="logo-img" width={100} height={100} />
-            </Link>
-          </div>
-          <ul className="hidden md:flex list-none gap-2.1rem">
-            <li>
-              <Link className="mr-4" href="/">
-                Home
-              </Link>
-            </li>
-            <li>
-              <Link className="mx-4" href="/about">
-                About
-              </Link>
-            </li>
-            <li>
-              <Link className="mx-4" href="/models">
-                Vehicle Models
-              </Link>
-            </li>
-            <li>
-              <Link className="mx-4" href="/TestimonialsPage">
-                Testimonials
-              </Link>
-            </li>
-            <li>
-              <Link className="mx-4" href="/team">
-                Our Team
-              </Link>
-            </li>
-            <li>
-              <Link className="ml-4" href="/contact">
-                Contact
-              </Link>
-            </li>
-          </ul>
-          <div className="navbar__buttons flex gap-2.5rem text-base font-medium items-center">
-            <Link
-              className="text-black cursor-pointer hover:text-orange transition-all duration-300 mx-10 "
-              href="/"
-            >
-              Sign In
-            </Link>
-            <Link
-              className="bg-orange-700 text-white px-3 py-1.5 rounded-md shadow transition-all duration-300 hover:shadow-md hover:bg-orange-400 "
-              href="/"
-            >
-              Register
-            </Link>
-          </div>
-
-          {/* mobile */}
-          <div
-            className="text-[2.8rem] md:hidden cursor-pointer transition-all duration-300 hover:text-orange-700"
-            onClick={openNav}
-          >
-            <FaBars />
-          </div>
-        </div>
-      </nav>
+      </header>
     </>
   );
 }
