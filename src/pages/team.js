ee7ab865528ef9e0656fc268b14844bb15efe229
shubh batra <godwithsam@gmail.com>
2023-06-24 02:17:28 +0100

    made changes to team page.

--- src/pages/team.js
+++ src/pages/team.js
@@ -1,21 +1,15 @@
 import HeroPages from '@/components/HeroPage';
 import React from 'react';
-
-import Person1 from '../assets/team/member1.jpg';
-import Person2 from '../assets/team/member2.jpg';
-import Person3 from '../assets/team/member3.jpg';
-import Person4 from '../assets/team/member4.jpg';
-import Person5 from '../assets/team/member5.jpg';
 import BookBanner from '@/components/BookBanner';
 
 
 const team = () => {
   const teamPpl = [
-    { img: Person1, name: "Julia Roberts", job: "Saleswoman" },
-    { img: Person2, name: "Joana W", job: "CFO" },
-    { img: Person3, name: "Stephanie", job: "Business Owner" },
-    { img: Person4, name: "Anubhav S", job: "Product Owner" },
-    { img: Person5, name: "Mahim K", job: "Web Developer" },
+    { img: 'assets/team/member1.jpg', name: "Julia Roberts", job: "Saleswoman" },
+    { img: 'assets/team/member2.jpg', name: "Joana W", job: "CFO" },
+    { img: 'assets/team/member3.jpg', name: "Stephanie", job: "Business Owner" },
+    { img: 'assets/team/member4.jpg', name: "Anubhav S", job: "Product Owner" },
+    { img: 'assets/team/member5.jpg', name: "Mahim K", job: "Web Developer" },
   ];
 
   return (
@@ -23,7 +17,7 @@ const team = () => {
       <HeroPages name="Our Team" />
       <div className='container'>
       <div className="grid grid-cols-3 grid-rows-auto gap-[4rem] items-center text-center px-[10rem] py-[2rem] w-[110rem] m-0 
-      lg:grid-cols-2 lg:w-fit-content md:grid-cols-1 md:w-fit-content">
+      ">
             {teamPpl.map((ppl, id) => (
               <div key={id} className="w-[33rem] bg-white shadow-xl sm:w-full">
                 <div className="w-full h-auto bg-white">
